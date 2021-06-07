9d8733681c3b62fd69eb1458c46f184f84029271
Stratosj <39690253+Stratosj@users.noreply.github.com>
2021-06-07 14:59:04 +0200

    Update GenerateOutlookEmailsWithRecipients.py
    Refined for iterrow.
    Not sure how to implement a better/faster way. Or actually any other way.
    No OOP.
    This is just a quick patch to fix once a year issue.

--- GenerateOutlookEmailsWithRecipients.py
+++ GenerateOutlookEmailsWithRecipients.py
@@ -2,19 +2,26 @@ import win32com.client
 import pandas as pd
 
 EXCEL_FILE = pd.read_excel(r"C:\Users\HR-training\Desktop\address_list.xlsx")  # File must be unlocked!
+#TODO: Make this dynamically reading excel in \Source folder.
 
 MAIL_SUBJECT = 'Employee Satisfaction Survey 2021 - Results'
 
-MAIL_BODY = """Dear colleagues,
+MAIL_BODY = """Vážení kolegové,
+
+děkujeme vám a vašim týmům za účast na Průzkumu spokojenosti zaměstnanců. Dalším krokem ke zvýšení spokojenosti zaměstnanců je vytvoření akčního plánu spolu s vašimi členy týmu. Za každý tým navrhněte 1-3 akční kroky, které povedou ke zlepšení spokojenosti. Tyto akční body se musí týkat přímo vašeho týmu, aby je byli schopni všichni členové ovlivnit. Termín každého akčního kroku si stanovíte sami, nejzazší je 31. října 2021. 
+Tyto akční plány dejte do přiloženého formuláře a zašlete přes business cooperation report managerovi a team chiefovi.
+
+Součástí tohoto akčního plánu je i možný návrh na zlepšení pro ostatní týmy. Tento návrh na zlepšení musí být dobře popsaný, musí poukázat, co konkrétně zlepší. 
+
+Předem děkujeme za spolupráci,
+
 
 """
 
 
-#TODO: Change to OOP?
-
 def send_outlook_mail(recipients, subject='No Subject', body='Blank', send_or_display='Display', copies=None):
     """
-    Send an Outlook Text email
+    Send an Outlook Text email to recipients and copy list
     :param recipients: list of recipients' email addresses (list object)
     :param subject: subject of the email
     :param body: body of the email
@@ -55,11 +62,11 @@ def send_outlook_mail(recipients, subject='No Subject', body='Blank', send_or_di
 
 if __name__ == '__main__':
 
-    for row in EXCEL_FILE.iterrows():
-
-        print(row
-        recipient_list = row
-
-        copies_list = ['simon.soukup@nexentire.com']
+    for i, row in EXCEL_FILE.iterrows():
+        recipient_list = []
+        recipient_list.append(row['E-mail'])
+        
+        copies_list = []
+        copies_list.append(row['E-mail - nadřízený'])
 
         send_outlook_mail(recipients=recipient_list, subject=MAIL_SUBJECT, body=MAIL_BODY, send_or_display='Display', copies=copies_list)
